bb78b13bb41ce008381e0989daec1ee62808b9a0
Justin Obney <justinobney@gmail.com>
2017-05-01 12:09:40 -0500

    step 1 - cleanup
    - add bootstrap css for easy styling
    - setup eslint config
    - setup editorconfig

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hi</div>, document.getElementById('root'));
+ReactDOM.render(
+  <div className="container">
+    <div className="starter-template">
+      <h1>Lets Learn React</h1>
+      <p className="lead">
+        Step 1 - CRA
+      </p>
+    </div>
+  </div>,
+  document.getElementById('root')
+);
