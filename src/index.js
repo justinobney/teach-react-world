fbf67244a25e08f1a18985490e76a7d6de700714
Justin Obney <justinobney@gmail.com>
2017-05-01 12:11:07 -0500

    step 2 - presentational component
    alert component that can change type

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Alert extends React.Component {
+  render() {
+    return (
+      <div className={`alert alert-${this.props.type}`} role="alert">
+        {this.props.title && <strong>{this.props.title} - </strong>}
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <div className="container">
     <div className="starter-template">
       <h1>Lets Learn React</h1>
-      <p className="lead">
-        Step 1 - CRA
-      </p>
+      <Alert title="Step 2" type="info">
+        Presentational Component
+      </Alert>
     </div>
   </div>,
   document.getElementById('root')
