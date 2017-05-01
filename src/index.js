34bad72bb91888cf844d101f7f86e972f8ffe59b
Justin Obney <justinobney@gmail.com>
2017-05-01 12:12:23 -0500

    step 4 - container component
    - convert root component to "Home" componenet
    - drive Alert type from Home state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Home extends React.Component {
+  state = {
+    type: 'info',
+  };
+
+  // 👀 binding this
+  _toggleAlertType = () => {
+    // 👀 setState callback
+    this.setState(prevState => ({
+      type: prevState.type === 'info' ? 'danger' : 'info',
+    }));
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="starter-template">
+          <h1>Lets Learn React</h1>
+          {/* 👀 callback function */}
+          <button className="btn btn-default" onClick={this._toggleAlertType}>
+            Toggle Alert Type
+          </button>
+          <hr />
+          {/* 👀 use of state passed as props */}
+          <Alert title="Step 2" type={this.state.type}>
+            Presentational Component
+          </Alert>
+        </div>
+      </div>
+    );
+  }
+}
+
 const Alert = props => (
   <div className={`alert alert-${props.type}`} role="alert">
     {props.title && <strong>{props.title} - </strong>}
@@ -8,14 +41,4 @@ const Alert = props => (
   </div>
 );
 
-ReactDOM.render(
-  <div className="container">
-    <div className="starter-template">
-      <h1>Lets Learn React</h1>
-      <Alert title="Step 2" type="info">
-        Presentational Component
-      </Alert>
-    </div>
-  </div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Home />, document.getElementById('root'));
