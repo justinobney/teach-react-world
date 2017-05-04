5587e839e6a6282eb3b08b19159feb62cf1a7b2e
Justin Obney <justinobney@gmail.com>
2017-05-04 13:59:50 -0500

    chore: reset components for next examples

--- src/index.js
+++ src/index.js
@@ -1,42 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Even = ({children}) => (
-  <h3><span className="label label-success">{children} - Even</span></h3>
-);
-
-const Odd = ({children}) => (
-  <h3><span className="label label-info">{children} - Odd</span></h3>
-);
-
-const Switch = ({count, children = Function()}) => (
-  <div>
-    {children(count % 2 === 0)}
-  </div>
-);
-
-class App extends React.Component {
-  state = {
-    count: 0,
-  };
-  incrementCount = () => {
-    this.setState(prevState => ({count: prevState.count + 1}));
-  };
-  render() {
-    return (
-      <div>
-        <button className="btn btn-default" onClick={this.incrementCount}>
-          Click Me
-        </button>
-        <Switch count={this.state.count}>
-          {isEven =>
-            (isEven
-              ? <Even>{this.state.count}</Even>
-              : <Odd>{this.state.count}</Odd>)}
-        </Switch>
-      </div>
-    );
-  }
-}
+const App = () => <div>App reset</div>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
