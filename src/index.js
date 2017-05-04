ebfc93d5d036c413c45b2b0d3fcf2cde7f198e66
Justin Obney <justinobney@gmail.com>
2017-05-03 22:23:46 -0500

    chore: reset components for next examples

--- src/index.js
+++ src/index.js
@@ -1,47 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Panel = ({children}) => (
-  <div className="panel panel-default">
-    <div className="panel-body">
-      {children}
-    </div>
-  </div>
-);
-
-const Well = ({children}) => <div className="well">{children}</div>;
-
-// 👀 default value is a component
-const Section = ({as = Panel, renderContainer, children}) => {
-  // 👀 just renaming to be more explicit
-  // <as>foo...</as> would have worked as well
-  if (typeof renderContainer === 'function') {
-    return renderContainer(children);
-  }
-
-  let Component = as;
-  return <Component>{children}</Component>;
-};
-
-const App = () => (
-  <div className="container">
-    <Section>Default will be panel</Section>
-
-    {/* 👀 passing in component as property */}
-    <Section as={Well}>Can over-ride to "Well"</Section>
-
-    {/* 👀 passing in render function as property */}
-    <Section
-      renderContainer={children => (
-        <div className="jumbotron">
-          <h1>Look ma!</h1>
-          {children}
-        </div>
-      )}
-    >
-      Can over-ride via renderContainer for more control
-    </Section>
-  </div>
-);
+const App = () => <div>App go here</div>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
