22f595f50f53a23ddd358c929e471ceb2a632917
Justin Obney <justinobney@gmail.com>
2017-05-03 21:57:02 -0500

    step 11 - compoent as prop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>App go here</div>;
+const Panel = ({children}) => (
+  <div className="panel panel-default">
+    <div className="panel-body">
+      {children}
+    </div>
+  </div>
+);
+
+const Well = ({children}) => <div className="well">{children}</div>;
+
+// 👀 default value is a component
+const Section = ({as = Panel, children}) => {
+  // 👀 just renaming to be more explicit
+  // <as>foo...</as> would have worked as well
+  let Component = as;
+  return <Component>{children}</Component>;
+};
+
+const App = () => (
+  <div className="container">
+    <Section>Default will be panel</Section>
+
+    {/* 👀 passing in component as property */}
+    <Section as={Well}>Can over-ride to "Well"</Section>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
